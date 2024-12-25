11e9b53496e1944fde8899d15701791d9119811e
David Wang <124479657+fdformula@users.noreply.github.com>
2024-12-25 13:38:49 -0500

    Add files via upload

---
+++ browser-setting.lua
@@ -0,0 +1,28 @@
+-- no firefox? you are strongly encouraged to install it. (www.mozilla.org)
+-- ms edge doesn't work well
+win_browser = 'C:/Program Files/Mozilla Firefox/firefox.exe'
+linux_browser = 'firefox'
+mac_browser = 'firefox'
+
+-- plotly_engine = 'plotly-2.35.2.min.js'
+plotly_engine = 'file:///C:/cygwin/bin/plotly-2.9.0.min.js'
+--plotly_engine = 'file:////usr/local/share/lua/5.4/plotly-2.9.0.min.js'
+
+
+
+
+
+-- ↓ ↓ ↓ ↓ Do nothing below unless you know what you do!!! ↓ ↓ ↓ ↓ --
+--
+-- temporary plotting output, a HTML file of JavaScript code and data
+-- using plotly graphing library (https://plotly.com/javascript/)
+--
+-- the file will stay until you delete it manually or so.
+local home = os.getenv("HOME")
+if home == nil then
+  print('Please define your HOME directory and come back.\n')
+  os.exit()
+else
+  home = string.gsub(home, '\\', '/')
+  temp_plot_html_file = home .. '/_tmp-plotlyjs_2-35_2.html'
+end
