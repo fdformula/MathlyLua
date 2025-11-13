fc82808f2aa84a37e2fc28dc4c08255b04e673f8
David Wang <124479657+fdformula@users.noreply.github.com>
2025-11-12 21:52:24 -0500

    Add files via upload

--- examples/explore-file-system.lua
+++ examples/explore-file-system.lua
@@ -3,14 +3,17 @@ mathly = require('mathly')
 clear()
 
 -- explore a folder and all subfolders for files specified in 'specs'
--- e.g., explore('/usr/share/', '*.lua'), explore('/', {'*.lua', 'fd*.jl'})
--- process(fname, i, n) is a function that defines how to process the i-th
+-- opts.process(fname, i, n) is a function that defines how to process the i-th
 -- one of all n files
 --
 -- 1. no change is needed for function explore(...)
 -- 2. edit function process(...) at the bottom of this file for your task
 --
-function explore(dir, specs, process, opt)
+function explore(dir, specs, opts)
+  if opts == nil then opts = {} end
+  if opts.folder == nil then opts.folder = false end
+  if opts.subfolder == nil then opts.subfolder = true end
+
   if type(specs) == 'string' then
     specs = {specs}
   elseif specs == nil then
@@ -18,9 +21,10 @@ function explore(dir, specs, process, opt)
   end
   local fname, cmd = tmp_plot_html_file .. 'XpL'
   if iswindows() then -- dir /s /b /a-d /on *.lua mathly*
-    cmd = 'dir /s /b /on '
-    cmd = cmd .. qq(opt == 'directory' or opt == 'folder', '/ad', '/a-d')
-    for i = 1, #specs do cmd = cmd .. ' ' .. specs[i] end
+    cmd = 'dir /b /on '
+    if opts.subfolder then cmd = cmd .. '/s ' end
+    cmd = cmd .. qq(opts.folder, '/ad', '/a-d')
+    for i = 1, #specs do cmd = cmd .. ' "' .. specs[i] .. '"' end
     cmd = cmd .. ' > ' .. fname
 
     local batfname = fname .. '.bat'
@@ -31,25 +35,25 @@ function explore(dir, specs, process, opt)
     os.execute(batfname)
     rm(batfname)
   else -- find . -type f \( -name "*.lua" -o -name "mathly*" \)
-    cmd = 'find "' .. dir .. '" -type '
-    cmd = cmd .. qq(opt == 'directory' or opt == 'folder', 'd', 'f')
+    cmd = 'find "' .. dir .. '" '
+    if opts.subfolder == false then cmd = cmd .. '-maxdepth 1 ' end
+    cmd = cmd .. '-type ' .. qq(opts.folder, 'd', 'f')
     cmd = cmd .. ' \\( -name'
     for i = 1, #specs do
       if i > 1 then cmd = cmd .. ' -o -name' end
-      cmd = cmd .. ' ' .. specs[i]
+      cmd = cmd .. ' "' .. specs[i] .. '"'
     end
     cmd = cmd .. ' \\) > ' .. fname
   end
 
-  local v, fnames = os.execute(cmd), {}
-  if v == true then
+  local fnames, v = {}, os.execute(cmd)
+  if v then
     local f = io.open(fname, "r")
     for txt in f:lines() do fnames[#fnames + 1] = txt end
     f:close()
     rm(fname)
-  else
-    print("Failed to explore the folder.")
-    return -1
+  --else
+  --  print("No entry was found.")
   end
 
   local i = 1 -- remove repetitive entries from fnames, a SORTED list
@@ -61,7 +65,7 @@ function explore(dir, specs, process, opt)
 
   local n = #fnames
   for i = 1, n do
-    if type(process) == 'function' then process(fnames[i], i, n) end
+    if type(opts.task) == 'function' then opts.task(fnames[i], i, n) end
   end
   fnames = nil
   printf("\n%d items processed\n", n)
@@ -90,7 +94,7 @@ function parse_filename(fname)
 end
 
 --
--- define how to process the i-th one of all n files/folders
+-- define how to process fname, the i-th one of all n files/folders
 --
 function process(fname, i, n)
   local k, fmt = n, 1
@@ -98,14 +102,18 @@ function process(fname, i, n)
   fmt = string.format('No. %%%dd of %%d: %%s\n', fmt)
   printf(fmt, i, n, fname)
 
-  -- your task
+  -- process fname here ...
 end
 
 -- examples
 if iswindows() then
-  explore('C:\\cygwin', {'*.lua', 'mathly*'}, process)           -- list & process files only
-  explore('C:\\cygwin', {'doc*', 'cuda*'}, process, 'folder')    -- list and process folders only
+  explore('C:\\cygwin', {'*.lua', 'mathly*'},
+          {task = process, folder = false, subfolder = true})  -- process files, including those in subfolders
+  explore('C:\\cygwin', {'doc*', 'cuda*'},
+          {task = process, folder = true, subfolder = true})   -- process folder and subfolders only
 else
-  explore('/usr/local/', {'mathly*', '*.txt'}, process)          -- list & process files only
-  explore('/usr/share/cudatext', 'cuda_*', process, 'directory') -- list & process folders only
+  explore('/usr/share/doc', {'mathly*', 's*', '*.html'},
+          {task = process, folder = false, subfolder = false}) -- process files only, excluding those in subfolders
+  explore('/usr/share/cudatext', 'cuda_*',
+          {task = process, folder = true, subfolder = true})   -- process folder and subfolders only
 end
