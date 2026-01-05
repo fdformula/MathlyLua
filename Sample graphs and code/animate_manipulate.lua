8d58953245742db55bed76fe02cccdb429f02bb0
David Wang <124479657+fdformula@users.noreply.github.com>
2026-01-04 23:56:09 -0500

    Update animate_manipulate.lua

--- Sample graphs and code/animate_manipulate.lua
+++ Sample graphs and code/animate_manipulate.lua
@@ -91,22 +91,46 @@ opts = {N = {1, MaxN, 1, default = MaxN}, x = {-0.1, 1.1}, y = {-0.1, 1.12}, con
 manipulate(fstr, opts)
 
 -- manipulate4.jpg
+--↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ cobweb for x = g(x) ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓--
+g = '1 - sqrt(x)'  -- g(x) = 1 - sqrt(x); divergent? g = '1-x^2'
+MaxIterations = 30
+--↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ cobweb for x = g(x) ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑--
 jscode = [[
-  function g(x) { return 1 - x*x; }
-  var xs = [0.5]; // 0.5, initial value
-  function displaytext() { return 'Iteration ' + I + ': x = ' + xs[I-1]; }
+  function g(x) { return %s; }
+  var xs = [a]; // xs[i] == g(xs[i-1]); jscript index starts at 0 & xs[0] is extra
   for (let I = 0; I <= %d; I += 1) { xs.push( g(xs[I]) ); } // one extra element, xs[0]!
+
+  function plotweb() {
+    let style = { 'color': 'grey', 'width': 1 }
+    for (let i = 2; i <= I; i++) {
+      mthlyTraces.push({x: [ xs[i-2], xs[i-1] ], y: [ xs[i-1], xs[i-1] ], mode: 'lines', line: style}); // horizontal line
+      mthlyTraces.push({x: [ xs[i-1], xs[i-1] ], y: [ xs[i-1], xs[i]   ], mode: 'lines', line: style}); // vertical line
+    }
+
+    // plot enough extra objects outside the graph so that no old traces will stay - ugly, but works!
+    for (let i = 1; i <= mthlyIMax * 4; i ++) {
+      mthlyTraces.push({ x: [mthlyxMax + i], y: [mthlyyMax + i], mode: 'markers' })
+    }
+  }
+
+  function displaytext() { plotweb(); return 'x<sub>' + (I-1) + '</sub> = ' + xs[I-1]; }
 ]]
 
-MaxI = 30 -- range of control I: [1, MaxI]
-fstr = {{x = {'xs[I-2]', 'xs[I-1]'}, y = {'xs[I-1]', 'xs[I-1]'}, line = true, color = 'grey', width = 1}, -- horizontal line segment
-        {x = {'xs[I-1]', 'xs[I-1]'}, y = {'xs[I-1]', 'xs[I]'}, line = true, color = 'grey', width = 1}} -- vertical line segment
-opts = {t = {-0.1, 1.1, 0.001}, I = {1, MaxI, 1, default = MaxI}, x = {-0.1, 1.1}, y = {-0.1, 1.1},
-        layout = { width = 540, height = 540, square = true, title = "" },
-        javascript = string.format(jscode, MaxI), cumulative = true, controls = 'I',
-        enhancements = {{x = '@(t) t', y = '@(t) t', t = {-1, 6}, width = 2, color = 'green'},   -- line: y = x
-                        {x = {'xs[0]', 'xs[0]'}, y = {0, 'xs[1]'}, line = true, width = 1, color = 'grey'}, -- line: line segment
-                        {x = '@(t) t', y = '@(t) 1 - t^2', color = 'orange'},                    -- curve: y = g(x)
-                        {x = 'xs[I-1]', y = 'g(xs[I-1])', color = 'red', size = 8, point = true} -- point
-                       }}
+fstr = '@(x) ' .. g
+opts = {
+  I = {1, MaxIterations, 1, default = MaxIterations, label = 'Iterations'}, x = {-0.1, 1.1}, y = {-0.1, 1.1},
+  a = {0, 1, 0.1, default = 0.9, label = 'Initial Value'},
+  layout = {
+    width = 540, height = 540, square = true,
+    title = "<h3>Cobweb of g(x) = " .. g .. '</h3>'
+  },
+  javascript = string.format(jscode, _to_jscript_expr(g), MaxIterations), controls = 'aI',
+  enhancements = {
+    {x = '@(t) t', y = '@(t) t', t = {0, 1}, width = 2, color = 'green'}, -- line: y = x
+    {x = {'xs[0]', 'xs[0]'}, y = {0, 'xs[1]'}, line = true, width = 1, color = 'grey'},
+    {x = 'xs[I-1]', y = 'g(xs[I-1])', color = 'red', size = 8, point = true},
+    {x = 'xs[I-1]', y = 0, color = 'red', size = 8, point = true},
+    {x = 'xs[I-1]', y = -0.02, color = 'black', size = 12, text = "x<sub>' + (I-1) + '</sub>"}
+  }
+}
 manipulate(fstr, opts)
