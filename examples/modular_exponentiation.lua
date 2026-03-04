d8a6aa8055ea9cf2108d3e61cfd2d291a102131e
David Wang <124479657+fdformula@users.noreply.github.com>
2026-03-04 18:50:21 -0500

    Add files via upload

--- examples/modular_exponentiation.lua
+++ examples/modular_exponentiation.lua
@@ -1,6 +1,7 @@
+-- calculate b^n mod m, i.e., mod(b^n, m)
 mathly = require('mathly')
 
--- find b^n mod m
+-- version 1
 function recursive_modular_exp(b, n, m)
   if n == 0 then
     return 1
@@ -16,3 +17,18 @@ function recursive_modular_exp(b, n, m)
 end
 
 recursive_modular_exp(350, 20260303, 23)
+
+
+-- version 2
+function binary_modular_exp(b, n, m)
+  local a = dec2bin(n)
+  local x, power = 1, mod(b, m)
+  for i = #a, 1, -1 do
+    if a:sub(i, i) == '1' then x = mod(x * power, m) end
+    power = mod(power * power, m)
+  end
+  return x
+end
+
+binary_modular_exp(350, 20260303, 23)
+
